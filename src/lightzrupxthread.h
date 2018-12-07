70b1dc1aa90d6ed6c8e27c7b8ce1f9abf6fe8b88
mo-bay <mabaig@me.com>
2018-12-07 17:59:20 +0000

    ///

--- src/lightzrupxthread.h
+++ src/lightzrupxthread.h
@@ -83,7 +83,7 @@ private:
                 } else {
                     LogPrintf("%s calculating work for %s \n\n", "rupx-light-thread", genWit.toString());
                     int blockHeight = pIndex->nHeight;
-                    if (blockHeight >= pIndex->nHeight()) {
+                    // if (pIndex->nHeight) {
 
                         // TODO: The protocol actually doesn't care about the Accumulator..
                         libzerocoin::Accumulator accumulator(params, genWit.getDen(), genWit.getAccWitValue());
